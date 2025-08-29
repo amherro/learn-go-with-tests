74e1c1333374b1e5dd09842b1c1be75ef245608d
Adam Herro <adam.herro@comcast.net>
2025-08-29 17:24:22 -0400

    More work with slices

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("Sum of slices (no fixed size)", func(t *testing.T) {
@@ -11,6 +14,15 @@ func TestSum(t *testing.T) {
 
 		if got != want {
 			t.Errorf("Got %d want %d given %v", got, want, numbers)
+		}
+	})
+
+	t.Run("Sum of multiple slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v want %v", got, want)
 		}
 	})
 }
