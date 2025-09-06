b33ab5725ff5092d6f8b241ff313d74b54982aa7
Adam Herro <adam.herro@comcast.net>
2025-09-06 14:20:06 -0400

    More work with slices and creating a function to handle assertions in the tests.

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -6,6 +6,12 @@ import (
 )
 
 func TestSum(t *testing.T) {
+	checkSum := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v want %v", got, want)
+		}
+	}
 	t.Run("Sum of slices (no fixed size)", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
@@ -20,9 +26,18 @@ func TestSum(t *testing.T) {
 	t.Run("Sum of multiple slices", func(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
 		want := []int{3, 9}
+		checkSum(t, got, want)
+	})
+	t.Run("Sum all tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Got %v want %v", got, want)
-		}
+		checkSum(t, got, want)
+	})
+	t.Run("Safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSum(t, got, want)
 	})
 }
