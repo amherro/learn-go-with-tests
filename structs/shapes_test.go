8610a36c1887e3b1d00759d4752c58bf00d6dac1
Adam Herro <adam.herro@comcast.net>
2025-09-09 17:39:50 -0400

    Refactoring tests and naming struct fields.

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -39,17 +39,21 @@ func TestPerimeter(t *testing.T) {
 // Table Driven Tests
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{12.0, 6.0}, 72.0},
-		{Circle{10.0}, 314.1592653589793},
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("Got %g want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v Got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
