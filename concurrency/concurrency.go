1c1780d5c1b084a407ec1e3773a8132de68b6b9a
Adam Herro <adam.herro@comcast.net>
2026-05-01 22:40:28 -0400

    Finishing Concurrency lesson

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,17 +1,26 @@
 package main
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
+	resultChan := make(chan result)
+
 	for _, url := range urls {
 		go func() {
-			results[url] = wc(url)
+			// results[url] = wc(url)
+			resultChan <- result{url, wc(url)}
 		}()
 	}
-	time.Sleep(2 * time.Millisecond)
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChan
+		results[r.string] = r.bool
+	}
 	return results
 }
