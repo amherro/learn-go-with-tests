a2ccdde7439b593d1f12b573514d621963e1d16f
Adam Herro <adam.herro@comcast.net>
2026-05-04 17:07:39 -0400

    Starting select lesson. Making basic http server and httptest server

---
+++ select/select.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string) {
+
+	durationA := measureResponseTime((a))
+	durationB := measureResponseTime(b)
+
+	if durationA < durationB {
+		return a
+	}
+
+	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	startTime := time.Now()
+	http.Get(url)
+	duration := time.Since(startTime)
+
+	return duration
+}
---
+++ select/select_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	slowUrl := slowServer.URL
+	fastUrl := fastServer.URL
+
+	want := fastUrl
+	got := Racer(slowUrl, fastUrl)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
